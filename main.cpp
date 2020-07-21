fb6683dbaa1874f56518c1d21335cfedbce284e3
れみろう <remyroez+github@gmail.com>
2020-07-21 23:17:57 +0900

    :sparkles: Add plugin manager

--- main.cpp
+++ main.cpp
@@ -5,11 +5,55 @@
 #include "cr.h"
 
 #include <filesystem>
+#include <vector>
 
 namespace
 {
-    
-cr_plugin ctx;
+
+// Plug-In Manager
+class dear_plugin_manager
+{
+public:
+    // Open new plugin
+    bool open(const char *path) {
+        cr_plugin plugin;
+        plugin.userdata = _userdata;
+        bool success = cr_plugin_open(plugin, path);
+        if (success) {
+            _plugins.push_back(plugin);
+        }
+        return success;
+    }
+
+    // Close all plugin
+    void close() {
+        for (auto &plugin : _plugins) {
+            cr_plugin_close(plugin);
+        }
+        _plugins.clear();
+    }
+
+    // Update all plugin
+    void update() {
+        for (auto &plugin : _plugins) {
+            cr_plugin_update(plugin);
+        }
+    }
+
+    // Set user data
+    void set_userdata(void *userdata) {
+        _userdata = userdata;
+    }
+
+private:
+    // plugins
+    std::vector<cr_plugin> _plugins;
+
+    // user data
+    void *_userdata = NULL;
+};
+
+dear_plugin_manager plugin_manager;
 
 } // namespace
 
@@ -40,14 +84,14 @@ int main(int, char**)
     }
 
     // Setup cr
-    ctx.userdata = ImGui::GetCurrentContext();
-    cr_plugin_open(ctx, "plugins/" CR_PLUGIN("example"));
+    plugin_manager.set_userdata(ImGui::GetCurrentContext());
+    plugin_manager.open("plugins/" CR_PLUGIN("example"));
 
     // start
     ImApp::StartMainLoop(main_loop);
 
     // cleanup
-    cr_plugin_close(ctx);
+    plugin_manager.close();
     ImApp::EndApplication();
 
     return 0;
@@ -58,5 +102,5 @@ void main_loop(void* arg)
 {
     IM_UNUSED(arg);
 
-    cr_plugin_update(ctx);
+    plugin_manager.update();
 }
